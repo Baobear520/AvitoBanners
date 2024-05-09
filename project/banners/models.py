e80bf379f01a3278aad4aa3acfea9a2eb0877ea5
Dima <aldmikon@gmail.com>
2024-05-09 11:15:44 +0800

    13th commit Restructuring schema and adding models

--- project/banners/models.py
+++ project/banners/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User as BaseUser
+from django.forms import ValidationError
 class Tag(models.Model):
     """Model class for tags"""
     pass
@@ -9,23 +10,37 @@ class Feature(models.Model):
     """Model class for features"""
     pass
 
+
 class Banner(models.Model):
     """Model class for banners"""
 
-    def content_data():
-        return {'title':"",'text':"",'url':""}
-    
-    tags = models.ManyToManyField(Tag,verbose_name="list_of_tags")
-    feature = models.ForeignKey(Feature,on_delete=models.CASCADE)
-    content = models.JSONField(default = content_data())
+    tags = models.ManyToManyField(Tag,through='BannerTagFeature')
+    feature = models.ForeignKey(Feature,on_delete=models.CASCADE,related_name='banners')
+    content = models.JSONField(default=dict)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
+class BannerTagFeature(models.Model):
+    """Intermediate model to enforce the unique constraint for a tag and a feature"""
     
-class User(models.Model):
+    tag = models.ForeignKey(Tag,on_delete=models.CASCADE)
+    banner = models.ForeignKey(Banner, on_delete=models.CASCADE)
+    feature = models.ForeignKey(Feature, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['tag','banner','feature'], 
+                name='unique_banner',
+                violation_error_message="A banner must have a unique tag and feature"
+                )
+        ]
+
+class UserBanner(BaseUser):
     """Model class for users"""
-    username = models.CharField(max_length=64,unique=True)
+    
     use_last_revision = models.BooleanField(default=False)
-    user_tag = models.ForeignKey(Tag,on_delete=models.CASCADE)
+    user_tag = models.ForeignKey(Tag,on_delete=models.CASCADE,verbose_name='tag_id')
     
