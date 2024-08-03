f66a261446412fd45ec7f89891c5f46fd5216755
Dima <aldmikon@gmail.com>
2024-08-03 14:03:44 +0800

    37th commit Adding /auth/ urls to the main urlconf

--- project/config/urls.py
+++ project/config/urls.py
@@ -5,6 +5,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/',include('core.urls')),
     path('',include('banners.urls')),
     path('__debug__/',include(debug_toolbar.urls))
 ] 
