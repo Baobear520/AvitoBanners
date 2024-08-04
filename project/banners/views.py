f4d9e2843628ad0da6b531105229b594a1c7f916
Dima <aldmikon@gmail.com>
2024-08-04 14:24:36 +0800

    39th commit - Refactoring exception handleling - Refactoring the UserBanner view (adding get_object() method)

--- project/banners/views.py
+++ project/banners/views.py
@@ -1,12 +1,12 @@
-from django.forms import ValidationError
 from django.db import transaction
 from django.http import Http404
 
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny,IsAdminUser,IsAuthenticated
+from rest_framework.permissions import IsAdminUser,IsAuthenticated
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.exceptions import ValidationError,APIException
 
 from banners.models import Banner, BannerTagFeature, UserBanner
 from banners.serializers import BannerSerializer, BannerTagFeatureSerializer, ProfileSerializer, UpdateBannerSerializer
@@ -22,39 +22,45 @@ class BannerList(APIView):
     permission_classes = [IsAdminUser]
     
     def get(self, request):
-        tag_id = request.query_params.get('tag_id')
-        feature_id = request.query_params.get('feature_id')
+        try:
+            tag_id = request.query_params.get('tag_id')
+            feature_id = request.query_params.get('feature_id')
 
-        # Initial queryset
-        queryset = Banner.objects.all()
+            # Initial queryset
+            queryset = Banner.objects.all()
 
-        # Apply filtering based on query parameters
-        if tag_id and feature_id:
-            queryset = queryset.filter(tags__id=tag_id, feature__id=feature_id)
-        elif tag_id:
-            queryset = queryset.filter(tags__id=tag_id)
-        elif feature_id:
-            queryset = queryset.filter(feature__id=feature_id)
-        
-        # Prefetch related tags and features
-        queryset = queryset.prefetch_related('tags', 'feature')
-        
-        if not queryset.exists():
+            # Apply filtering based on query parameters
+            if tag_id and feature_id:
+                queryset = queryset.filter(tags__id=tag_id, feature__id=feature_id)
+            elif tag_id:
+                queryset = queryset.filter(tags__id=tag_id)
+            elif feature_id:
+                queryset = queryset.filter(feature__id=feature_id)
+            
+            # Prefetch related tags and features
+            queryset = queryset.prefetch_related('tags', 'feature')
+
+            # Apply pagination
+            paginator = LimitOffsetPagination()
+            page = paginator.paginate_queryset(queryset, request)
+
+            # Serialize data
+            serializer = BannerSerializer(page, many=True)
+
+            # Return paginated response
+            return paginator.get_paginated_response(serializer.data)
+
+        except APIException as e:
+                return Response(
+                    {"error": str(e)},
+                    status=e.status_code
+                )
+        except Exception as e:
+            # Log the exception if needed
             return Response(
-                {"error": "No banner found matching the given parameters"},
-                status=status.HTTP_404_NOT_FOUND
-            )
-
-        # Apply pagination
-        paginator = LimitOffsetPagination()
-        page = paginator.paginate_queryset(queryset, request)
-
-        # Serialize data
-        serializer = BannerSerializer(page, many=True)
-
-        # Return paginated response
-        return paginator.get_paginated_response(serializer.data)
-        
+                {"error": "An unexpected error occurred."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
         
     def post(self, request):
         data = request.data
@@ -95,7 +101,16 @@ class BannerList(APIView):
             if banner.id:
                 banner.delete()
             return Response({'error': e.message}, status=status.HTTP_400_BAD_REQUEST)
-
+        except APIException as e:
+            return Response(
+                {"error": str(e)},
+                status=e.status_code
+                    )
+        except Exception as e:
+            # Log the exception if needed
+            return Response(
+                {"error": "An unexpected error occurred."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class BannerDetail(APIView):
     """Update/delete a banner"""
@@ -106,11 +121,23 @@ class BannerDetail(APIView):
             return Banner.objects.get(pk=pk)
         except Banner.DoesNotExist:
             raise Http404
+
         
     def get(self,request,pk):
-        banner = self.get_object(pk)
-        serializer = BannerSerializer(banner)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+        try:
+            banner = self.get_object(pk)
+            serializer = BannerSerializer(banner)
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        except APIException as e:
+                return Response(
+                    {"error": str(e)},
+                    status=e.status_code
+                )
+        except Exception as e:
+            # Log the exception if needed
+            return Response(
+                {"error": "An unexpected error occurred."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         
     def patch(self, request, pk):
@@ -143,13 +170,35 @@ class BannerDetail(APIView):
 
         except ValidationError as e:
             return Response({'error': e.detail}, status=status.HTTP_400_BAD_REQUEST)
+        
+        except APIException as e:
+            return Response(
+                {"error": str(e)},
+                status=e.status_code
+                    )
         except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            # Log the exception if needed
+            return Response(
+                {"error": "An unexpected error occurred."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
 
     def delete(self, request, pk):
-        banner = self.get_object(pk)
-        banner.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        try:
+            banner = self.get_object(pk)
+            banner.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except APIException as e:
+            return Response(
+                {"error": str(e)},
+                status=e.status_code
+            )
+        except Exception as e:
+            # Log the exception if needed
+            return Response(
+                {"error": "An unexpected error occurred."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
 
 class UserBannerView(APIView):
@@ -160,32 +209,42 @@ class UserBannerView(APIView):
     
     permission_classes = [IsAuthenticated]
 
+    def get_object(self, tag_id, feature_id):
+        """
+        Get the Banner object based on the tag_id and feature_id.
+        """
+        try:
+            return Banner.objects.get(tags__id=tag_id, feature__id=feature_id)
+        except Banner.DoesNotExist:
+            raise Http404("No banner matching the given parameters found.")
+
     def get(self, request):
         tag_id = request.query_params.get('tag_id')
         feature_id = request.query_params.get('feature_id')
-        use_last_revision = request.query_params.get('use_last_revision', 'False').lower()==True
 
         if not tag_id or not feature_id:
             return Response(
-                {"error": "Incorrect data. Please provide correct tag_id and feature_id"}, 
-                status=status.HTTP_400_BAD_REQUEST)
-        
-        # if use_last_revision:
-        #     do smth
-        # else: 
-        # do smth else
-        
+                {"error": "Incorrect data. Please provide both correct tag_id and feature_id"}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
         try:
-            queryset = Banner.objects.get(tags__id=tag_id, feature__id=feature_id)
-            serializer = BannerSerializer(queryset)
-            return Response(serializer.data['content'],status=status.HTTP_200_OK)
+            banner = self.get_object(tag_id, feature_id)
+            serializer = BannerSerializer(banner)
+            return Response(serializer.data['content'], status=status.HTTP_200_OK)
         
-        except Banner.DoesNotExist:
+        except APIException as e:
             return Response(
-                {"error": "No banner matching the given parameters found"},
-                status=status.HTTP_404_NOT_FOUND)
-        
-        
+                {"error": str(e)},
+                status=e.status_code
+            )
+        except Exception as e:
+            # Log the exception if needed
+            return Response(
+                {"error": "An unexpected error occurred."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+         
 class ProfileList(APIView):
     
     """
@@ -199,27 +258,51 @@ class ProfileList(APIView):
     
  
     def get(self,request):
+        try:
+            queryset = UserBanner.objects.all()
+            users = ProfileSerializer(queryset,many=True)
+            return Response(data=users.data,status=status.HTTP_200_OK)
         
-        queryset = UserBanner.objects.all()
-        users = ProfileSerializer(queryset,many=True)
-        return Response(data=users.data,status=status.HTTP_200_OK)
+        except APIException as e:
+            return Response(
+                {"error": str(e)},
+                status=e.status_code
+            )
+        except Exception as e:
+            # Log the exception if needed
+            return Response(
+                {"error": "An unexpected error occurred."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
     
     def post(self, request):
-        user = AvitoUser.objects.get(pk=request.user.id)
+        try:
+            user = AvitoUser.objects.get(pk=request.user.id)
 
-        # Check if the user already has a profile
-        if UserBanner.objects.filter(user=user).exists():
-            return Response({"detail": "Profile already exists."}, status=status.HTTP_400_BAD_REQUEST)
+            # Check if the user already has a profile
+            if UserBanner.objects.filter(user=user).exists():
+                return Response({"detail": "Profile already exists."}, status=status.HTTP_400_BAD_REQUEST)
+            
+            # Create the profile
+            data = request.data
+            serializer = ProfileSerializer(data=data)
+            serializer.is_valid(raise_exception=True)
+            profile = serializer.save(user=user)
+            
+            return Response(ProfileSerializer(profile).data, status=status.HTTP_201_CREATED)
         
-        # Create the profile
-        data = request.data
-        serializer = ProfileSerializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        profile = serializer.save(user=user)
+        except APIException as e:
+            return Response(
+                {"error": str(e)},
+                status=e.status_code
+            )
+        except Exception as e:
+            # Log the exception if needed
+            return Response(
+                {"error": "An unexpected error occurred."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
         
-        return Response(ProfileSerializer(profile).data, status=status.HTTP_201_CREATED)
-
 class ProfileDetail(APIView):
     """
     Show details of the banner servise user
@@ -235,23 +318,44 @@ class ProfileDetail(APIView):
             raise Http404
         
     def get(self,request,pk):
-
-        user = self.get_object(pk)
-        self.check_object_permissions(request, user)
-        serializer = ProfileSerializer(user)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+        try:
+            user = self.get_object(pk)
+            self.check_object_permissions(request, user)
+            serializer = ProfileSerializer(user)
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        
+        except APIException as e:
+            return Response(
+                {"error": str(e)},
+                status=e.status_code
+            )
+        except Exception as e:
+            # Log the exception if needed
+            return Response(
+                {"error": "An unexpected error occurred."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def patch(self,request, pk):
+        try:
+            user = self.get_object(pk)
+            data = request.data
+            self.check_object_permissions(request, user)
+            serializer = ProfileSerializer(user,data=data,partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
 
-        user = self.get_object(pk)
-        data = request.data
-        self.check_object_permissions(request, user)
-        serializer = ProfileSerializer(user,data=data,partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response(serializer.data,status=status.HTTP_200_OK)
-
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        
+        except APIException as e:
+                return Response(
+                    {"error": str(e)},
+                    status=e.status_code
+                )
+        except Exception as e:
+            # Log the exception if needed
+            return Response(
+                {"error": "An unexpected error occurred."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 
